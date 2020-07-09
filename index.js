3987b5605fab0d2dbedf62f86a074eb807b61d66
Roman <romazukov1993@gmail.com>
2020-07-09 16:21:37 +0200

    Created methods to kill and alive a specific cell

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 export const cells = [
-    [{x: 1, y: 1}, {x: 1, y: 2}, {x: 1, y: 3}, {x: 1, y: 4}, {x: 1, y: 5}, {x: 1, y: 6}],
-    [{x: 2, y: 1}, {x: 2, y: 2}, {x: 2, y: 3}, {x: 2, y: 4}, {x: 2, y: 5}, {x: 2, y: 6}],
-    [{x: 3, y: 1}, {x: 3, y: 2}, {x: 3, y: 3}, {x: 3, y: 4}, {x: 3, y: 5}, {x: 3, y: 6}],
-    [{x: 4, y: 1}, {x: 4, y: 2}, {x: 4, y: 3}, {x: 4, y: 4}, {x: 4, y: 5}, {x: 4, y: 6}],
-    [{x: 5, y: 1}, {x: 5, y: 2}, {x: 5, y: 3}, {x: 5, y: 4}, {x: 5, y: 5}, {x: 5, y: 6}],
-    [{x: 6, y: 1}, {x: 6, y: 2}, {x: 6, y: 3}, {x: 6, y: 4}, {x: 6, y: 5}, {x: 6, y: 6}]
+    [{x: 1, y: 1, alive: false}, {x: 1, y: 2, alive: false}, {x: 1, y: 3, alive: false}, {x: 1, y: 4, alive: false}, {x: 1, y: 5, alive: false}, {x: 1, y: 6, alive: false}],
+    [{x: 2, y: 1, alive: false}, {x: 2, y: 2, alive: false}, {x: 2, y: 3, alive: false}, {x: 2, y: 4, alive: false}, {x: 2, y: 5, alive: false}, {x: 2, y: 6, alive: false}],
+    [{x: 3, y: 1, alive: false}, {x: 3, y: 2, alive: false}, {x: 3, y: 3, alive: false}, {x: 3, y: 4, alive: false}, {x: 3, y: 5, alive: false}, {x: 3, y: 6, alive: false}],
+    [{x: 4, y: 1, alive: false}, {x: 4, y: 2, alive: false}, {x: 4, y: 3, alive: false}, {x: 4, y: 4, alive: false}, {x: 4, y: 5, alive: false}, {x: 4, y: 6, alive: false}],
+    [{x: 5, y: 1, alive: false}, {x: 5, y: 2, alive: false}, {x: 5, y: 3, alive: false}, {x: 5, y: 4, alive: false}, {x: 5, y: 5, alive: false}, {x: 5, y: 6, alive: false}],
+    [{x: 6, y: 1, alive: false}, {x: 6, y: 2, alive: false}, {x: 6, y: 3, alive: false}, {x: 6, y: 4, alive: false}, {x: 6, y: 5, alive: false}, {x: 6, y: 6, alive: false}]
 ];
+
+export const killCell = (x, y) => {
+    cells[x - 1][y - 1].alive = false;
+};
+
+export const aliveCell = (x, y) => {
+    cells[x - 1][y - 1].alive = true;
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,15 +1,25 @@
 import {test, expect} from "@jest/globals";
-import {cells} from "../index";
+import * as index from "../index";
 
 test('The Y axis length should be 6', () => {
-    expect(cells.length).toBe(6);
+    expect(index.cells.length).toBe(6);
 });
 
-test('The X axis length should be 6', () => {
-    expect(cells[0].length).toBe(6);
-    expect(cells[1].length).toBe(6);
-    expect(cells[2].length).toBe(6);
-    expect(cells[3].length).toBe(6);
-    expect(cells[4].length).toBe(6);
-    expect(cells[5].length).toBe(6);
+test('The X axis length of each Y position should be 6', () => {
+    expect(index.cells[0].length).toBe(6);
+    expect(index.cells[1].length).toBe(6);
+    expect(index.cells[2].length).toBe(6);
+    expect(index.cells[3].length).toBe(6);
+    expect(index.cells[4].length).toBe(6);
+    expect(index.cells[5].length).toBe(6);
+});
+
+test('The killCell method should set the "alive" value to "false" for a specific cell', () => {
+    index.killCell(1, 1);
+    expect(index.cells[0][0].alive).toBe(false);
+});
+
+test('The aliveCell method should set the "alive" value to "true" for a specific cell', () => {
+    index.aliveCell(1, 1);
+    expect(index.cells[0][0].alive).toBe(true);
 });
