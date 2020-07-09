76b9f690deabcdcb28223cb3efc85f9bf7f5c203
Roman <romazukov1993@gmail.com>
2020-07-09 16:01:58 +0200

    Added babel.config.js for jest, created cells array.

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,15 @@
 import {test, expect} from "@jest/globals";
+import {cells} from "../index";
 
-test('test', () => {
-    
+test('The Y axis length should be 6', () => {
+    expect(cells.length).toBe(6);
+});
+
+test('The X axis length should be 6', () => {
+    expect(cells[0].length).toBe(6);
+    expect(cells[1].length).toBe(6);
+    expect(cells[2].length).toBe(6);
+    expect(cells[3].length).toBe(6);
+    expect(cells[4].length).toBe(6);
+    expect(cells[5].length).toBe(6);
 });
